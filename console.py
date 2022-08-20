67c4e674c7c191e20bb2ebd87337ddf8f8f01ecf
Bruce Johnston <brucejohnston@ymail.com>
2022-08-20 16:54:07 +0100

    updates

--- console.py
+++ console.py
@@ -3,11 +3,11 @@ import pdb
 from models.member import Member
 import repositories.member_respository as member_repository
 
-from models.session import Session
-import repositories.session_repository as session_repository
+from models.gym_session import Session
+import repositories.gym_session_repository as gym_session_repository
 
-from models.member_in_session import MemberInSession
-import repositories.member_in_session_repository as member_in_session_repository
+from models.members_in_session import MemberInSession
+import repositories.members_in_session_repository as members_in_session_repository
 
 
 
@@ -26,21 +26,21 @@ member3 = Member('Nick Parker')
 member_repository.save(member3)
 
 session1 = Session('Spinning')
-session_repository.save(session1)
+gym_session_repository.save(session1)
 
 session2 = Session('Yoga')
-session_repository.save(session2)
+gym_session_repository.save(session2)
 
 session3 = Session('Boxing')
-session_repository.save(session3)
+gym_session_repository.save(session3)
 
 member_in_session1 = MemberInSession(member3, session2)
-member_in_session_repository.save(member_in_session1)
+members_in_session_repository.save(member_in_session1)
 
 member_in_session2 = MemberInSession(member2, session2)
-member_in_session_repository.save(member_in_session2)
+members_in_session_repository.save(member_in_session2)
 
 member_in_session3 = MemberInSession(member1, session3)
-member_in_session_repository.save(member_in_session3)
+members_in_session_repository.save(member_in_session3)
 
 pdb.set_trace()
