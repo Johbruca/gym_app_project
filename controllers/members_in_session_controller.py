89976e8a6d3b073ebd60cd4327487956e0f83b81
Bruce Johnston <brucejohnston@ymail.com>
2022-08-21 11:40:33 +0100

    live site...

--- controllers/members_in_session_controller.py
+++ controllers/members_in_session_controller.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.members_in_session import MemberInSession
-import repositories.members_in_session_repository as member_in_session_repository
+import repositories.members_in_session_repository as members_in_session_repository
 import repositories.member_respository as member_repository
 import repositories.gym_session_repository as gym_session_repository
 
-visits_blueprint = Blueprint("members_in_sessions", __name__)
+members_in_session_blueprint = Blueprint("members_in_sessions", __name__)
