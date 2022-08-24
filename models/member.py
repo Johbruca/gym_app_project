1abe2c7e9147642f855b6bc05e88c7c06636e89c
Bruce Johnston <brucejohnston@ymail.com>
2022-08-24 19:24:36 +0100

    starting unittesting

--- models/member.py
+++ models/member.py
@@ -2,3 +2,6 @@ class Member:
     def __init__(self, name, id=None):
         self.name = name
         self.id = id
+
+    def equals(self, member):
+        return member.name == self.name
--- tests/gym_sessions_test.py
+++ tests/gym_sessions_test.py
@@ -1,7 +1,13 @@
 import unittest
 
-from models.member import Mmeber
+from models.member import Member
 from models.bookings import Bookings
 from models.gym_session import GymSession
 
 class GymSession(unittest.TestCase):
+
+    def setUp(self):
+        self.gym_session = GymSession("Boxing")
+
+    def test_session_has_name(self):
+        self.assertEqual("Yoga", self.gym_session.description)
