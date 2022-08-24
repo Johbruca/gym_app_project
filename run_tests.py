429b5ce8e17053326ff5cac61dc5777e7c7ef7a6
Bruce Johnston <brucejohnston@ymail.com>
2022-08-24 14:57:19 +0100

    tests

---
+++ run_tests.py
@@ -0,0 +1,7 @@
+import unittest
+from tests.gym_sessions_test import TestGymSession
+from tests.member_test import TestMember
+from tests.bookings_test import TestBookings
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/bookings_test.py
@@ -0,0 +1,7 @@
+import unittest
+
+from models.member import Mmeber
+from models.bookings import Bookings
+from models.gym_session import GymSession
+
+class Bookings(unittest.TestCase):
