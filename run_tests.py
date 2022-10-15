73e70696fed9c5546b88ce7b4f4e1ee78a091211
Bruce Johnston <brucejohnston@ymail.com>
2022-10-15 11:11:44 +0100

    refactoring

--- run_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from tests.gym_session_test import TestGymSession
-from tests.member_test import TestMember
-from tests.bookings_test import TestBookings
-
-if __name__ == '__main__':
-    unittest.main()
